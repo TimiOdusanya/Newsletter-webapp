97fed3da1bb7cb704870c16ab130e1cbb25b74ab
Timi Odusanya <timmycruz36@gmail.com>
2022-03-12 14:01:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/948efa90b";
+
+  const options = {
+    method: "POST",
+    auth: "timi1:952d57dcfa470b71a11d93e4e8bc14fd-us14"
+  }
+
+const request = https.request(url, options,function(response){
+
+  let statusCode = response.statusCode;
+  console.log(statusCode);
+  if(statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+
+  }else{
+      res.sendFile(__dirname + "/failure.html");
+  }
+  response.on("data",function(data){
+    //console.log(JSON.parse(data));
+  })
+})
+
+request.write(jsonData);
+request.end();
+});
+
+app.post("/failure.html", function(req,res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port 3000");
+})
+
+//API Key
+// 952d57dcfa470b71a11d93e4e8bc14fd-us14
+
+//Unique Id
+// 948efa90be
